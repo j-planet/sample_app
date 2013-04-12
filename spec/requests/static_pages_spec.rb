f3eb0c379488d497a574734a37db274d457c0a7b
jennyyuejin <jin.yuejenny@gmail.com>
2013-04-11 18:37:37 -0700

    refactored title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,6 +9,12 @@ describe "Static Pages" do
       page.should have_content('Sample App')
     end
 
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                  :text => "Ruby on Rails Tutorial Sample App | Home")
+    end
+
   end
 
   describe "Help page" do
@@ -16,12 +22,24 @@ describe "Static Pages" do
       visit '/static_pages/help'
       page.should have_content('Help')
     end
+
+    it "should have the right title" do
+      visit '/static_pages/help'
+      page.should have_selector('title',
+                                :text => "Ruby on Rails Tutorial Sample App | Help")
+    end
   end
 
   describe "About page" do
     it "should have content 'About Us'" do
       visit '/static_pages/about'
       page.should have_content('About Us')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/about'
+      page.should have_selector('title',
+                                :text => "Ruby on Rails Tutorial Sample App | About Us")
     end
   end
 end
