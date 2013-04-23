33c41052b8dd899a910640fc91a38ade68b4d02f
jennyyuejin <jin.yuejenny@gmail.com>
2013-04-23 16:30:06 -0700

    finished chapter 9 except conclusion

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -8,6 +8,7 @@ describe 'Users' do
     let(:user) { FactoryGirl.create(:user) }
 
     before(:each) do
+      visit signin_path
       sign_in user
       visit users_path
     end
@@ -17,15 +18,35 @@ describe 'Users' do
 
     describe "pagination" do
 
-      before(:all) { 30.times { FactoryGirl.create(:user) } }
+      before(:all) { 15.times { FactoryGirl.create(:user) } }
       after(:all) { User.delete_all }
 
       it { should have_selector('div.pagination') }
 
       it "should list each user" do
-        User.paginate(page: 1).each do |user|
+        User.paginate(page: 1, per_page: 15).each do |user|
           page.should have_selector('li', text: user.name)
         end
+      end
+    end
+
+    describe "delete links" do
+
+      it {should_not have_link('delete') }
+
+      describe "as an admin user" do
+        let(:admin) { FactoryGirl.create(:admin) }
+        before do
+          visit signin_path
+          sign_in admin
+          visit users_path
+        end
+
+        it { should have_link('delete', href: user_path(User.first)) }
+        it "should be able to delete another user" do
+          expect { click_link('delete')}.to change(User, :count).by(-1)
+        end
+        it { should_not have_link('delete', href: user_path(admin))}
       end
     end
 
@@ -107,6 +128,7 @@ describe 'Users' do
   describe "edit" do
     let(:user) { FactoryGirl.create(:user) }
     before do
+      visit signin_path
       sign_in user
       visit edit_user_path(user)
     end
